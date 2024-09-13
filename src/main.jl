793ad921f887f73eddda5e46a303800050cc37a2
SajjiKazemi <SajjadKazemi.e@gmail.com>
2024-09-13 12:07:17 -0400

    Add the boundary conditions and use the toolbox API function to define the boundary conditions.

--- src/main.jl
+++ src/main.jl
@@ -46,5 +46,24 @@ end
 wrap(func) = (t, k, x, u, p, pbm) -> func(t, x, u, p)
 problem_set_dynamics!(pbm, wrap(f), wrap(A), wrap(B), wrap(F))
 
+## Define the boundary conditions when car doesn't reverse
+x_0 = zeros(3)
+x_f = [0; 2; 0]
+
+## Define the boundary conditions when car reverses
+# x_0 = [0; 0.5; 0]
+# x_f = [0; 1.5; 0]
+
+g_ic(x, p) = x - x_0
+g_tc(x, p) = x - x_f;
+
+## Define Jacobians for g_ic and g_tc
+H_0(x, p) = I(3)
+H_f(x, p) = I(3);
+
+## Use the toolbox API function for defining the boundary conditions
+wrap(func) = (x, p, pbm) -> func(x, p)
+problem_set_bc!(pbm, :ic, wrap(g_ic), wrap(H_0))
+problem_set_bc!(pbm, :tc, wrap(g_tc), wrap(H_f))
 
 
