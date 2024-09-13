38bca3b86c9d280d80629d32c932f7d5ac4c6bac
SajjiKazemi <SajjadKazemi.e@gmail.com>
2024-09-13 11:50:16 -0400

    Add the problem dimensions and define the dynamics.

--- src/main.jl
+++ src/main.jl
@@ -6,3 +6,19 @@ using PyPlot, Colors, LinearAlgebra
 
 using ECOS
 
+## Initialize the trajectory optimization problem
+pbm = TrajectoryProblem()
+
+## Define the problem dimensions
+n, m, d = 3, 2, 1
+problem_set_dims!(pbm, n, m, d)
+
+## Define the dynamics
+t_f = 3
+f(t, x, u, p) = begin
+    x, y, theta = x
+    v, w = u
+    return [v*sin(theta); v*cos(theta); w]*t_f
+end;
+
+
