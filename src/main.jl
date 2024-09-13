06059fc6fef559eb8d3a9aa567db32d52a1451ed
SajjiKazemi <SajjadKazemi.e@gmail.com>
2024-09-13 11:57:30 -0400

    Add the Jacobians of the dynamics.

--- src/main.jl
+++ src/main.jl
@@ -21,4 +21,26 @@ f(t, x, u, p) = begin
     return [v*sin(theta); v*cos(theta); w]*t_f
 end;
 
+## Define the Jaconbians of the dynamics
+A(t, x, u, p) = begin
+    x, y, theta = x
+    v, w = u
+    return [0.0 0.0 v*cos(theta);
+            0.0 0.0 -v*sin(theta);
+            0.0 0.0 0.0]*t_f
+end
+
+B(t, x, u, p) = begin
+    x, y, theta = x
+    v, w = u
+    return [sin(theta) 0.0;
+            cos(theta) 0.0;
+            0.0 1.0]*t_f
+end
+
+F(t, x, u, p) = begin
+    return zeros(3, 1)
+end
+
+
 
