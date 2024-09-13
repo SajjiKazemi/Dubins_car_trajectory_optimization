7f87db568b19de60956704d642cd5b99bf866183
SajjiKazemi <SajjadKazemi.e@gmail.com>
2024-09-13 12:21:11 -0400

    Add the nonconvex obstacle constraint and determine the SCP algorithm type.

--- src/main.jl
+++ src/main.jl
@@ -42,7 +42,7 @@ F(t, x, u, p) = begin
     return zeros(3, 1)
 end
 
-## Use the toolbox API function for defining the dynamics
+## Use the toolbox API function which is problem_set_dynamics! for defining the dynamics
 wrap(func) = (t, k, x, u, p, pbm) -> func(t, x, u, p)
 problem_set_dynamics!(pbm, wrap(f), wrap(A), wrap(B), wrap(F))
 
@@ -61,9 +61,25 @@ g_tc(x, p) = x - x_f;
 H_0(x, p) = I(3)
 H_f(x, p) = I(3);
 
-## Use the toolbox API function for defining the boundary conditions
+## Use the toolbox API function which is problem_set_bc! for defining the boundary conditions
 wrap(func) = (x, p, pbm) -> func(x, p)
 problem_set_bc!(pbm, :ic, wrap(g_ic), wrap(H_0))
 problem_set_bc!(pbm, :tc, wrap(g_tc), wrap(H_f))
 
+## Define the nonconvex obstacle constraint
+c_0 = [-0.1; 1]
+r_0 = 0.35
+car_width = 0.1
+delta_r_theta = car_width/2
+E_xy = [1 0 0; 0 1 0]
+s(t, x, u, p) = [(r_0 + delta_r_theta)^2 - (E_xy*(x - c_0))'*(E_xy*(x - c_0))];
 
+## Define the Jacobian of the nonconvex obstacle constraint
+C(t, x, u, p) = reshape(2*E_xy'*(c_0 - E_xy*x), 1, 3);
+
+## Use the toolbox API function which is problem_set_s! for defining the nonconvex obstacle constraint
+# We should also determine the SCP algorithm type
+#alg = :scvx
+alg = :ptr
+wrap(func) = (t, k, x, u, p, pbm) -> func(t, x, u, p)
+problem_set_s!(pbm, alg, wrap(s), wrap(C))
